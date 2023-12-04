16a429bda465a350a87b581f5d6645415ce12506
Paula Shin <paulashin15@gmail.com>
2023-12-04 15:03:08 -0500

    added pagination icons

--- js/script.js
+++ js/script.js
@@ -1,12 +1,5 @@
-var swiper = new Swiper(".mySwiper", {
-    cssMode: true,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-      el: ".swiper-pagination",
-    },
-    mousewheel: true,
-    keyboard: true,
-  });
+import { animate } from "motion"
+
+animate (
+    
+)
