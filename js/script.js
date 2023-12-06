3c53d5322d3319c319c0a9d969dfa0f7171b78c8
Paula Shin <paulashin15@gmail.com>
2023-12-06 15:25:24 -0500

    updated scroll snap

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-import { animate, scroll } from "motion"
 
-// scroll(animate(".progress", { scaleX: [0, 1] }));
 
-document.querySelectorAll("section").forEach((section) => {
-//   const header = section.querySelector("h2");
-  scroll(animate(header, { y: [-400, 400] }), {
-    target: header
-  });
+
+const memoji = document.getElementById("memoji");
+
+
+memoji.addEventListener('load', () => {
+    memoji.style.transform: rotate(45 deg);
 });
+
