e2c851d02e4aaf407344eb92fae361fe7836279b
Paula Shin <paulashin15@gmail.com>
2023-12-08 16:56:45 -0500

    fixed footer and scroll snap issues

--- js/script.js
+++ js/script.js
@@ -1,10 +1,34 @@
+// var img = document.getElementById("memojis");
+// var s = Snap(img);
+// var memoji = Snap.select('#memoji');
+// var memojiWave = Snap.select('#memoji-wave');
 
+// var memojiPoints = memoji.node.getAttribute('d');
+// var memojiWavePoints = memojiWave.node.getAttribute('d');
 
+// var toFancy = function(){
+//   memoji.animate({ d: memojiWavePoints }, 1000, mina.backout, toSimple);  
+// }
 
-const memoji = document.getElementById("memoji");
+// var toSimple = function(){
+//   memojiWave.animate({ d: memojiPoints }, 1000, mina.backout, toFancy); 
+// }
 
+// toSimple();
 
-memoji.addEventListener('load', () => {
-    memoji.style.transform: rotate(45 deg);
+document.addEventListener("DOMContentLoaded", function () {
+  var memoji = document.getElementById("memoji");
+  var memojiWave = document.getElementById("memoji-wave");
+
+  var toFancy = function () {
+    memoji.setAttribute("href", "assets/memoji-wave.svg");
+    setTimeout(toSimple, 1000);
+  }
+
+  var toSimple = function () {
+    memoji.setAttribute("href", "assets/memoji.svg");
+    setTimeout(toFancy, 1000);
+  }
+
+  toSimple();
 });
-
